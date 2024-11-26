53f2e8fb6edad92414f020cd1be4098bee4c48cc
David Petkov <davidpetkov2006@gmail.com>
2024-11-26 15:44:22 +0200

    Update

--- lyuinsyweb/src/pages/ItemPurchaseDirect.jsx
+++ lyuinsyweb/src/pages/ItemPurchaseDirect.jsx
@@ -4,6 +4,7 @@ import Jewelry2 from "../assets/images/6.png";
 import Jewelry3 from "../assets/images/5.png";
 import { useEffect } from 'react';
 import ScrollAnimation from "../components/ScrollAnimation";
+import { useState } from "react";
 
 const items = [
   {
@@ -62,6 +63,39 @@ export default function ItemPurchaseDirect() {
     window.scrollTo(0, 0); // Scroll to the top
   }, []);
 
+
+  const [currentUpsell, setCurrentUpsell] = useState(0);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+      const handleResize = () => {
+          setIsSmallScreen(window.innerWidth < 1500);
+      };
+
+      // Set initial value and add resize listener
+      handleResize();
+      window.addEventListener("resize", handleResize);
+
+      return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+      if (isSmallScreen) {
+          const interval = setInterval(() => {
+              setCurrentUpsell((prev) => (prev === upsellItems.length - 1 ? 0 : prev + 1));
+          }, 5000);
+          return () => clearInterval(interval);
+      }
+  }, [isSmallScreen, upsellItems.length]);
+
+  const handlePrevUpsell = () => {
+      setCurrentUpsell((prev) => (prev === 0 ? upsellItems.length - 1 : prev - 1));
+  };
+
+  const handleNextUpsell = () => {
+      setCurrentUpsell((prev) => (prev === upsellItems.length - 1 ? 0 : prev + 1));
+  };
+
   return (
     <ScrollAnimation>
     <div className="px-4 py-20 flex flex-col md:flex-row">
@@ -83,38 +117,124 @@ export default function ItemPurchaseDirect() {
           </div>
         </div>
   
-        {/* Upsell Items with Timer and Discount */}
         <div className="px-12">
-          <h3 className="text-3xl font-serif mb-8 text-gray-900">Limited Time Offers</h3>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            {upsellItems.map((upsell) => (
-              <div
-                key={upsell.id}
-                className="bg-white rounded-lg shadow-sm border overflow-hidden p-6 transition transform hover:scale-105"
-              >
-                <img
-                  src={upsell.image}
-                  alt={upsell.title}
-                  className="w-full h-48 object-cover rounded-lg"
-                />
-                <div className="mt-4">
-                  <h4 className="text-xl font-serif text-gray-900 mb-2">
-                    {upsell.title}
-                  </h4>
-                  <div className="flex justify-between items-center">
-                    <p className="text-xl font-bold text-gray-800 line-through">${upsell.price}</p>
-                    <p className="text-xl font-bold text-emerald-700">${upsell.price - (upsell.price * 0.2)}</p>
-                  </div>
-                  <p className="text-sm text-gray-500">Discounted price available for the next 5 minutes.</p>
-                  <div className="mt-4">
-                    <button className="bg-emerald-700 text-white px-6 py-2 rounded-full hover:bg-emerald-800 transition duration-300">
-                      Add to Order
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+            <h3 className="text-3xl font-serif mb-8 text-gray-900">Limited Time Offers</h3>
+            <div className="relative max-w-5xl mx-auto">
+                {isSmallScreen ? (
+                    <div className="overflow-hidden">
+                        <div
+                            className="flex transition-transform duration-500 ease-in-out"
+                            style={{ transform: `translateX(-${currentUpsell * 100}%)` }}
+                        >
+                            {upsellItems.map((upsell) => (
+                                <div key={upsell.id} className="min-w-full">
+                                    <div className="bg-white rounded-lg shadow-sm border overflow-hidden p-6">
+                                        <img
+                                            src={upsell.image}
+                                            alt={upsell.title}
+                                            className="w-full h-48 object-cover rounded-lg"
+                                        />
+                                        <div className="mt-4">
+                                            <h4 className="text-xl font-serif text-gray-900 mb-2">{upsell.title}</h4>
+                                            <div className="flex justify-between items-center">
+                                                <p className="text-xl font-bold text-gray-800 line-through">
+                                                    ${upsell.price}
+                                                </p>
+                                                <p className="text-xl font-bold text-emerald-700">
+                                                    ${upsell.price - upsell.price * 0.2}
+                                                </p>
+                                            </div>
+                                            <p className="text-sm text-gray-500">
+                                                Discounted price available for the next 5 minutes.
+                                            </p>
+                                            <div className="mt-4">
+                                                <button className="bg-emerald-700 text-white px-6 py-2 rounded-full hover:bg-emerald-800 transition duration-300">
+                                                    Add to Order
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                        {upsellItems.map((upsell) => (
+                            <div
+                                key={upsell.id}
+                                className="bg-white rounded-lg shadow-sm border overflow-hidden p-6 transition transform hover:scale-105"
+                            >
+                                <img
+                                    src={upsell.image}
+                                    alt={upsell.title}
+                                    className="w-full h-48 object-cover rounded-lg"
+                                />
+                                <div className="mt-4">
+                                    <h4 className="text-xl font-serif text-gray-900 mb-2">{upsell.title}</h4>
+                                    <div className="flex justify-between items-center">
+                                        <p className="text-xl font-bold text-gray-800 line-through">${upsell.price}</p>
+                                        <p className="text-xl font-bold text-emerald-700">
+                                            ${upsell.price - upsell.price * 0.2}
+                                        </p>
+                                    </div>
+                                    <p className="text-sm text-gray-500">
+                                        Discounted price available for the next 5 minutes.
+                                    </p>
+                                    <div className="mt-4">
+                                        <button className="bg-emerald-700 text-white px-6 py-2 rounded-full hover:bg-emerald-800 transition duration-300">
+                                            Add to Order
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+                {isSmallScreen && (
+                    <div className="flex justify-center gap-4 mt-6">
+                        <button
+                            onClick={handlePrevUpsell}
+                            className="bg-gray-100 p-2 rounded-full hover:bg-gray-200 transition"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M15 19l-7-7 7-7"
+                                />
+                            </svg>
+                        </button>
+
+                        <button
+                            onClick={handleNextUpsell}
+                            className="bg-gray-100 p-2 rounded-full hover:bg-gray-200 transition"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M9 5l7 7-7 7"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                )}
+            </div>
         </div>
       </div>
   
