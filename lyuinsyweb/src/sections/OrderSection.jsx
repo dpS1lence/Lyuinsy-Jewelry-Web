54fd1ca3054a787d3c9cb4b610dd8ef369c9027d
David Petkov <davidpetkov2006@gmail.com>
2024-11-28 20:03:24 +0200

    Fixed recaptcha error

--- lyuinsyweb/src/sections/OrderSection.jsx
+++ lyuinsyweb/src/sections/OrderSection.jsx
@@ -9,23 +9,23 @@ export default function OrderSection() {
     const handleRecaptchaChange = (value) => {
         setRecaptchaValue(value);
       };
-console.log(sitekeyRE);
-      const handleSubmit = (e) => {
-        e.preventDefault();
-      };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    };
 
     return (
         <div className="lg:w-1/3 bg-gray-50 lg:p-8 border-l border-gray-100">
         <h3 className="text-3xl font-serif mb-6 text-gray-900">Завършете Вашата Поръчка</h3>
         <form className="space-y-6" onSubmit={handleSubmit}>
           {/* Промо Код */}
-          <div className="flex">
+          <div className=" flex flex-row">
             <input
               type="text"
-              placeholder="Въведете вашия промо код"
-              className="flex-1 px-6 py-3 border border-gray-300 rounded-l-full text-gray-800 placeholder-gray-400"
+              placeholder="Промо код"
+              className="w-3/5 px-6 py-3 border border-gray-300 rounded-l-full text-gray-800 placeholder-gray-400"
             />
-            <button className="bg-emerald-700 text-white px-2 lg:px-6 py-3 rounded-r-full font-medium hover:bg-emerald-800">
+            <button className="w-2/5 bg-emerald-700 text-white px-2 lg:px-6 py-3 rounded-r-full font-medium hover:bg-emerald-800">
               Приложи
             </button>
           </div>
