4e626d088cf6a9e9100a2c07027c288bcf6ef7a6
David Petkov <davidpetkov2006@gmail.com>
2025-02-11 09:58:58 +0200

    Update appwrite

--- lyuinsyweb/src/lib/appwrite.js
+++ lyuinsyweb/src/lib/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Databases, ID, Storage } from "appwrite";
+import { Client, Databases, ID, Query } from "appwrite";
 
 export const appwriteConfig = {
   endpoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
@@ -21,37 +21,6 @@ client
   .setProject(appwriteConfig.projectId);
 
 const databases = new Databases(client);
-const storage = new Storage(client);
-
-// Upload file to storage
-export async function uploadFile(file) {
-  if (!file || !file.name) return null;
-
-  try {
-    const uploadedFile = await storage.createFile(
-      appwriteConfig.storageId,
-      ID.unique(),
-      file
-    );
-
-    const fileUrl = storage.getFilePreview(
-      appwriteConfig.storageId,
-      uploadedFile.$id,
-      2000,
-      2000,
-      "top",
-      100
-    );
-
-    return {
-      fileId: uploadedFile.$id,
-      fileUrl,
-    };
-  } catch (error) {
-    console.error("Error uploading file:", error);
-    throw error;
-  }
-}
 
 export async function saveEmail({email, date}){
   try {
@@ -97,68 +66,36 @@ export async function createOrder({userNames, userAdress, userPhone, userEmail, 
   }
 };
 
-// Create a new item
-export async function createItem({ name, description, oldPrice, actualPrice, discount, bulletsDescription, specialOffer, imageFile, quantity }) {
+// Get all items
+export async function getAllItems(limit = 10, offset = 0) {
   try {
-    // First upload the image
-    const { fileUrl } = await uploadFile(imageFile);
-
-    // Then create the item document
-    const item = await databases.createDocument(
+    const response = await databases.listDocuments(
       appwriteConfig.databaseId,
       appwriteConfig.itemsCollectionId,
-      ID.unique(),
-      {
-        name,
-        description,
-        old_price: oldPrice,
-        actual_price: actualPrice,
-        discount,
-        bullets_description: bulletsDescription || [],
-        special_offer: specialOffer,
-        image: fileUrl,
-        quantity
-      }
+      [Query.limit(limit), Query.offset(offset)]
     );
 
-    return item;
-  } catch (error) {
-    console.error("Error creating item:", error);
-    throw error;
-  }
-}
-
-// Get all items
-export async function getAllItems() {
-  try {
-    const items = await databases.listDocuments(
-      appwriteConfig.databaseId,
-      appwriteConfig.itemsCollectionId
-    );
-    
-    return items.documents;
+    return response.documents;
   } catch (error) {
     console.error("Error fetching items:", error);
     throw error;
   }
-}
+};
 
 export async function getOneItem(id) {
   try {
-    const items = await databases.listDocuments(
+    const item = await databases.getDocument(
       appwriteConfig.databaseId,
-      appwriteConfig.itemsCollectionId
+      appwriteConfig.itemsCollectionId,
+      id
     );
-
-    // Filter the collections to find the one with the given id
-    const item = items.documents.find(item => item.$id === id);
-    
-    return item || null; // Return the found collection or null if not found
+    return item;
   } catch (error) {
     console.error("Error fetching item:", error);
     throw error;
   }
-}
+};
+
 
 export async function getAllCollections() {
   try {
@@ -172,7 +109,7 @@ export async function getAllCollections() {
     console.error("Error fetching items:", error);
     throw error;
   }
-}
+};
 
 export async function getOneCollection(id) {
   try {
@@ -185,7 +122,7 @@ export async function getOneCollection(id) {
     const collection = collections.documents.find(collection => collection.$id === id);
     console.log(collection);
     // Fetch all items
-    const items = await getAllItems();
+    const items = await getAllItems(300, 0);
     console.log(items);
     
     // Add 'items' attribute to the collection with an array of items that belong to this collection
@@ -198,58 +135,8 @@ export async function getOneCollection(id) {
     console.error("Error fetching collection:", error);
     throw error;
   }
-}
-
-export const deleteItem = async (itemId) => {
-  try {
-    await databases.deleteDocument(
-      appwriteConfig.databaseId,
-      appwriteConfig.itemsCollectionId,
-      itemId
-    );
-    console.log("Item deleted successfully");
-  } catch (error) {
-    console.error("Error deleting item:", error);
-  }
 };
 
-export const updateItem = async (itemId, updatedData) => {
-  try {
-    console.log("Updating item with ID:", itemId);
-    let image = null;
-
-    // If there's a new image, upload it first
-    if (updatedData.imageFile) {
-      const { fileUrl } = await uploadFile(updatedData.imageFile);
-      image = fileUrl;
-    }
-
-    // Update the item document
-    const updatedItem = await databases.updateDocument(
-      appwriteConfig.databaseId,
-      appwriteConfig.itemsCollectionId,
-      itemId,
-      {
-        name: updatedData.name,
-        description: updatedData.description,
-        old_price: updatedData.oldPrice,
-        actual_price: updatedData.actualPrice,
-        discount: updatedData.discount,
-        bulletsDescription: updatedData.bulletsDescription || [],
-        special_offer: updatedData.specialOffer,
-        quantity: updatedData.quantity,
-        ...(image && { image }), // Only include image if a new image was uploaded
-      }
-    );
-
-    return updatedItem;
-  } catch (error) {
-    console.error("Error updating item:", error);
-    throw error;
-  }
-};
-
-// New function to decrement item quantity
 export const purchaseItem = async (itemId) => {
   try {
     const item = await getOneItem(itemId);
